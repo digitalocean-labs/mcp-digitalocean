347f04829545d57e29f3afc5e13a262db89be3e2
Shubham Singh <shubham.singh.200205@gmail.com>
2025-10-08 09:59:11 +0000

    feat(schema): automate generation of draft-07 schemas
    Modify generate.go to produce backward-compatible JSON schemas directly from the Go structs

--- go.mod
+++ go.mod
@@ -26,6 +26,9 @@ require (
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+	golang.org/x/mod v0.27.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/time v0.12.0 // indirect
+	golang.org/x/tools v0.36.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/apps/spec/generate.go
+++ internal/apps/spec/generate.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
 
 	"mcp-digitalocean/internal/apps"
-
+	
 	"github.com/digitalocean/godo"
 	"github.com/invopop/jsonschema"
+	// "github.com/wk8/go-ordered-map/v2" // added for ordered map
 )
 
 //go:generate go run .
@@ -17,58 +17,68 @@ import (
 // We generate the JSON schema from godo structs for AppCreateRequest and AppUpdateRequest.
 // This is necessary since we need to pass the AppSpec to the mcp tool as a raw argument.
 // Ideally, we shouldn't have to copy the godo files around. However, it's currently not possible to without preserving the struct comments.
+// modified the function to a direct way to fix the issue for schema 2020-12 problems and add the logic that intercepts the generated schema and changes its version to draft-07.
 func main() {
 	reflect := jsonschema.Reflector{
-		BaseSchemaID:               "",
-		Anonymous:                  true,
-		AssignAnchor:               false,
 		AllowAdditionalProperties:  true,
 		RequiredFromJSONSchemaTags: true,
-		DoNotReference:             true,
-		ExpandedStruct:             true,
-		FieldNameTag:               "",
 	}
-
 	err := reflect.AddGoComments("github.com/digitalocean/godo", "./")
 	if err != nil {
 		panic(fmt.Errorf("failed to add Go comments: %w", err))
 	}
 
-	createSchema, err := reflect.Reflect(&godo.AppCreateRequest{}).MarshalJSON()
+	// Generate schema for AppCreateRequest 
+	createSchemaObject := reflect.Reflect(&godo.AppCreateRequest{})
+	
+	createSchemaBytes, err := json.Marshal(createSchemaObject)
 	if err != nil {
 		panic(fmt.Errorf("failed to marshal app create schema: %w", err))
 	}
 
-	var createSchemaJSON bytes.Buffer
-	if err := json.Indent(&createSchemaJSON, createSchema, "", "  "); err != nil {
-		panic(fmt.Errorf("failed to indent JSON: %w", err))
+	var createSchemaMap map[string]interface{}
+	if err := json.Unmarshal(createSchemaBytes, &createSchemaMap); err != nil {
+		panic(fmt.Errorf("failed to unmarshal create schema to map: %w", err))
 	}
+	
+	createSchemaMap["$schema"] = "http://json-schema.org/draft-07/schema#"
 
-	// now write the schema to a file
-	err = os.WriteFile("./app-create-schema.json", createSchemaJSON.Bytes(), 0644)
+	finalCreateSchema, err := json.MarshalIndent(createSchemaMap, "", "  ")
+	if err != nil {
+		panic(fmt.Errorf("failed to marshal final create schema: %w", err))
+	}
+	
+	// Write the schema to a file
+	err = os.WriteFile("./app-create-schema.draft-07.json", finalCreateSchema, 0644)
 	if err != nil {
 		panic(fmt.Errorf("failed to write schema to file: %w", err))
 	}
+	fmt.Println("Draft-07 schema successfully written to app-create-schema.draft-07.json")
 
-	fmt.Println("Schema successfully written to app_create_schema.json")
 
-	// Generate schema for AppUpdateRequest
-	updateSchema, err := reflect.Reflect(&apps.AppUpdate{}).MarshalJSON()
+	// Generate schema for AppUpdateRequest 
+	updateSchemaObject := reflect.Reflect(&apps.AppUpdate{})
+	
+	updateSchemaBytes, err := json.Marshal(updateSchemaObject)
 	if err != nil {
 		panic(fmt.Errorf("failed to marshal app update schema: %w", err))
 	}
 
-	// Prettify the JSON
-	var updateSchemaJSON bytes.Buffer
-	if err := json.Indent(&updateSchemaJSON, updateSchema, "", "  "); err != nil {
-		panic(fmt.Errorf("failed to indent JSON: %w", err))
+	var updateSchemaMap map[string]interface{}
+	if err := json.Unmarshal(updateSchemaBytes, &updateSchemaMap); err != nil {
+		panic(fmt.Errorf("failed to unmarshal update schema to map: %w", err))
+	}
+	
+	updateSchemaMap["$schema"] = "http://json-schema.org/draft-07/schema#"
+	
+	finalUpdateSchema, err := json.MarshalIndent(updateSchemaMap, "", "  ")
+	if err != nil {
+		panic(fmt.Errorf("failed to marshal final update schema: %w", err))
 	}
 
-	// Write the schema to a file
-	err = os.WriteFile("./app-update-schema.json", updateSchemaJSON.Bytes(), 0644)
+	err = os.WriteFile("./app-update-schema.draft-07.json", finalUpdateSchema, 0644)
 	if err != nil {
 		panic(fmt.Errorf("failed to write schema to file: %w", err))
 	}
-
-	fmt.Println("Update schema successfully written to app_update_schema.json")
+	fmt.Println("Draft-07 update schema successfully written to app-update-schema.draft-07.json")
 }
